59285ce00043f03210f542747e9f19d9d262643c
caraldi <araldicm@Caitlin-M-Araldi.local>
2017-01-23 14:01:18 -0800

    Auth route signup

---
+++ server/lib/routes/auth.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const jsonParser = require('body-parser').json();
+const User = require('../models/user');
+const token = require('../auth/token');
+const ensureAuth = require('../auth/ensure-auth')();
+
+router.post('/signup', jsonParser, (req, res) => {
+  const {username, password} = req.body;
+  delete req.body.password;
+
+  if(!password) {
+    return res.status(400).json({
+      msg: 'Password is required'
+    });
+  }
+
+  User.findOne({username})
+    .then(existing => {
+      if (existing) {
+        return res.status(500).json({
+          msg: 'Invalid',
+          reason: 'Username already in use'
+        });
+      }
+
+      const user = new User(req.body);
+      user.generateHash(password);
+      return user.save()
+        .then(user => token.sign(user))
+        .then(token => res.json({token}));
+    })
+    .catch(err => {
+      res.status(500).json({
+        msg: 'Invalid',
+        reason: err
+      });
+    });
+});
+
