0d4adb0bbf0214c779412f251b3454fe72369fe1
caraldi <araldicm@Caitlin-M-Araldi.local>
2017-01-23 18:45:00 -0800

    Test user model

---
+++ server/test/user-model.test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = chai.assert;
+chai.use(chaiHttp);
+
+const connection = require('../lib/setup-mongoose');
+const db = require('./db');
+const app = require('../lib/app');
+
+describe('User model', () => {
+  it('Validates with username and password', done => {
+    const user = new User({
+      username: 'testuser',
+      password: 'testpass'
+    });
+
+    user.validate(err => {
+      if (!err) done();
+      else done(err);
+    });
+  });
+
+  it('Username is required', done => {
+    const user = new User({password: testpass});
+
+    user.validate(err => {
+      assert.isOk(err, 'Username is required');
+      done();
+    });
+  });
+
+  it('Password is required', done => {
+    const user = new User({username: testuser});
+
+    user.validate(err => {
+      assert.isOk(err, 'Password is required');
+      done();
+    });
+  })
+})
