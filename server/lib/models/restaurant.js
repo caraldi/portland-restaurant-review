62a4b0e40417b40ff54450bcfc8467555693549a
caraldi <araldicm@Caitlin-M-Araldi.local>
2017-01-26 19:00:35 -0800

    App webpack config

--- server/lib/models/restaurant.js
+++ server/lib/models/restaurant.js
@@ -2,15 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-  name: {type: String, required: true},
-  neighborhoodId: {type: Schema.Types.ObjectId, ref: 'Neighborhood', required: true},
-  address: {
-    street: {type: String, required: true}, 
-    city: {type: String, required: true}, 
-    zip: {type: String, required: true}, 
+  name: {
+    type: String, 
     required: true
   },
-  usernameId: {type: Schema.Types.ObjectId, ref: 'User', required: true}
+  neighborhoodId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Neighborhood',
+    required: true
+  },
+  address: {
+    street: String,
+    city: String,
+    zip: Number, 
+    required: true
+  },
+  usernameId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Restaurant', schema);
