4d3165afde8f662456a79d47fbd1b6588472078c
caraldi <araldicm@Caitlin-M-Araldi.local>
2017-01-26 19:44:51 -0800

    Add restaurant service

---
+++ app/src/services/restaurant-service.js
@@ -0,0 +1,14 @@
+restaurantService.$inject = ['$http', 'apiUrl'];
+
+export default function restaurantService($http, apiUrl) {
+  return {
+    add(restaurant) {
+      return $http.post(`${apiUrl}/restaurants`, restaurant)
+        .then(res => res.data);
+    },
+    remove(id) {
+      return $http.delete(`${apiUrl}/restaurants/${id}`)
+        .then(res => res.data);
+    }
+  };
+}
