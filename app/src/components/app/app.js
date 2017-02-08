569cb6604b05731fd12778ad6f8616cb332f7469
caraldi <araldicm@Caitlin-M-Araldi.local>
2017-02-08 15:06:58 -0800

    Added quadrants

--- app/src/components/app/app.js
+++ app/src/components/app/app.js
@@ -9,5 +9,5 @@ export default {
 controller.$inject = ['$state'];
 
 function controller($state) {
-  this.gotoNeighborhoods = () => $state.go('portland');
+  this.gotoQuadrants = () => $state.go('portland');
 }
